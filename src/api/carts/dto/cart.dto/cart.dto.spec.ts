9daa54ab961430f6584e9f94a951d0e8bf70ff89
wwspfernando <wwspfernando@students.nsbm.ac.lk>
2023-01-17 19:51:14 +0530

    Arrange the file structure

---
+++ src/api/carts/dto/cart.dto/cart.dto.spec.ts
@@ -0,0 +1,7 @@
+import { CartDto } from './cart.dto';
+
+describe('CartDto', () => {
+  it('should be defined', () => {
+    expect(new CartDto()).toBeDefined();
+  });
+});
---
+++ src/api/orders/dto/order.dto/order.dto.spec.ts
@@ -0,0 +1,7 @@
+import { OrderDto } from './order.dto';
+
+describe('OrderDto', () => {
+  it('should be defined', () => {
+    expect(new OrderDto()).toBeDefined();
+  });
+});
