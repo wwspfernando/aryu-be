25f212a62a8f61ef61f5e642c6f6db498f785b27
wwspfernando <wwspfernando@students.nsbm.ac.lk>
2023-01-20 10:40:13 +0530

    create port var

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
