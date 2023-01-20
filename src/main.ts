2fa7d468510f59a7fcab0ed4a769c83ca115a52e
wwspfernando <wwspfernando@students.nsbm.ac.lk>
2023-01-20 11:03:39 +0530

    update port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
 bootstrap();
